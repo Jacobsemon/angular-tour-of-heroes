2b89e2b2a604d128cbdadf7ea3f42a9804b03314
Clever Bear <cleverbear210@gmail.com>
2019-02-03 21:31:02 +0800

    all (#10)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopHeroesComponent } from './top-heroes/top-heroes.component';
+import { MyheroesComponent } from './myheroes/myheroes.component';
+import { EditheroesComponent } from './editheroes/editheroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopHeroesComponent,
+    MyheroesComponent,
+    EditheroesComponent
   ],
   imports: [
     BrowserModule
